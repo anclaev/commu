add9649914f5c721b5aec2c159eceebbb741a8a5
Artem Samarin <iahugo@yandex.ru>
2024-01-08 05:00:14 +0300

    Set up e2e.

--- apps/api-e2e/src/api/api.spec.ts
+++ apps/api-e2e/src/api/api.spec.ts
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
 describe('GET /api', () => {
-  it('should return a message', async () => {
-    const res = await axios.get(`/api`);
+  it('should return a 404 code', async () => {
+    const res = await axios.get(`/api`).catch((e) => e.response);
 
-    expect(res.status).toBe(200);
-    expect(res.data).toEqual({ message: 'Hello API' });
+    expect(res.status).toBe(404);
   });
 });

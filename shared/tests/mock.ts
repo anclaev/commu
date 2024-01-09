76251292a9a17f11395764d8b60bac06ba7dcfd2
Artem Samarin <iahugo@yandex.ru>
2024-01-09 11:28:13 +0300

    API - fix tests.

--- shared/tests/mock.ts
+++ shared/tests/mock.ts
@@ -4,6 +4,7 @@ export const mockEmployee: Omit<Employee, 'id'> = {
   login: 'test',
   name: 'test',
   password: 'test',
+  role: 'User',
   personal_key: 'test',
   rank: 'Private',
   salary: 1000,

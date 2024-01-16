b9d9c92e8d47c3ad98d789e640872709fb6cf8f9
Artem Samarin <iahugo@yandex.ru>
2024-01-16 05:47:30 +0300

    UI - work on auth form.

--- apps/ui/src/app/shared/shared.module.ts
+++ apps/ui/src/app/shared/shared.module.ts
@@ -1,8 +1,11 @@
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   imports: [CommonModule, ReactiveFormsModule],
+  declarations: [FormComponent],
+  exports: [FormComponent],
 })
 export class SharedModule {}

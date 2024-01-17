bbd8f9a0b316baca0104f03b291c1a62a3a5f580
Artem Samarin <iahugo@yandex.ru>
2024-01-17 05:28:00 +0300

    UI - sign in form.

--- apps/ui/src/app/auth/auth.module.ts
+++ apps/ui/src/app/auth/auth.module.ts
@@ -1,13 +1,16 @@
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 import { ReactiveFormsModule } from '@angular/forms';
+import { DialogModule } from '@angular/cdk/dialog';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { AuthComponent } from './auth.component';
 import { AuthService } from './auth.service';
 
+import { RecoverPasswordComponent } from './sign-in/recover-password/recover-password.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 
 @NgModule({
@@ -16,9 +19,11 @@ import { SignInComponent } from './sign-in/sign-in.component';
     ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
+    MatIconModule,
     MatButtonModule,
+    DialogModule,
   ],
   providers: [AuthService],
-  declarations: [AuthComponent, SignInComponent],
+  declarations: [AuthComponent, SignInComponent, RecoverPasswordComponent],
 })
 export class AuthModule {}
--- apps/ui/src/app/auth/sign-in/sign-in.component.ts
+++ apps/ui/src/app/auth/sign-in/sign-in.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Dialog } from '@angular/cdk/dialog';
+import { Component } from '@angular/core';
 
 import { MatSnackBar } from '@angular/material/snack-bar';
 
+import { RecoverPasswordComponent } from './recover-password/recover-password.component';
 import { AlarmComponent } from '../../shared/alarm/alarm.component';
+
 import { SNACKBAR_CONFIG } from '../../shared/types/snackbar-config';
 
 @Component({
@@ -14,21 +17,32 @@ import { SNACKBAR_CONFIG } from '../../shared/types/snackbar-config';
 export class SignInComponent {
   signInForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private sb: MatSnackBar) {
+  constructor(
+    private fb: FormBuilder,
+    private sb: MatSnackBar,
+    private dialog: Dialog
+  ) {
     this.signInForm = fb.group({
       login: ['', [Validators.required, Validators.minLength(5)]],
       password: ['', [Validators.required, Validators.minLength(5)]],
     });
   }
 
-  private showNotification() {
+  private showAlarm(data: string) {
     this.sb.openFromComponent(AlarmComponent, {
       ...SNACKBAR_CONFIG,
-      data: 'Авторизация пройдена',
+      data,
     });
   }
 
   signIn() {
-    this.showNotification();
+    this.showAlarm('Привет!');
+  }
+
+  recover() {
+    this.dialog.open(RecoverPasswordComponent, {
+      width: '90%',
+      maxWidth: '440px',
+    });
   }
 }

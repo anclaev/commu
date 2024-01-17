6e5d78029a6c33704851d117f269050f3b2505e5
Artem Samarin <iahugo@yandex.ru>
2024-01-17 10:24:58 +0300

    UI - fix nginx conf.

---
+++ apps/ui/src/app/auth/role.guard.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { inject } from '@angular/core';
+
+import {
+  ActivatedRouteSnapshot,
+  CanActivateFn,
+  RouterStateSnapshot,
+} from '@angular/router';
+
+import { AuthService } from './auth.service';
+
+export const roleGuard: CanActivateFn = (
+  _next: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+) => {
+  const authService = inject(AuthService);
+  const user = authService.currentUser;
+
+  if (user) return true;
+
+  return authService.check(state.url);
+};
